b2af96b6092efe136891e2cace1bcfd0549ab9c7
Mason MacDonald <masonmacdonald1997@gmail.com>
2023-04-28 02:39:07 -0400

    Gamepad setup and about population

--- js/shared.js
+++ js/shared.js
@@ -4,6 +4,10 @@ let aboutIcon = document.getElementById("about-link")
 let homeCard = document.getElementById("home")
 let gamesCard = document.getElementById("games")
 let aboutCard = document.getElementById("about")
+let startButton = document.getElementById("start")
+let gameInterface = document.getElementById("game-interface")
+let linkedin = document.getElementById("linkedin-pic")
+let github = document.getElementById("github-pic")
 
 homeIcon.addEventListener("click", () => {
     homeIcon.style.color = "grey"
@@ -31,6 +35,18 @@ aboutIcon.addEventListener("click", () => {
     homeCard.style.display = "none"
     gamesCard.style.display = "none"
     aboutCard.style.display = "inline"
+})
+
+startButton.addEventListener("click", () => {
+    gameInterface.innerHtml = "Start"
+})
+
+linkedin.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/mason-macdonald/")
+})
+
+github.addEventListener("click", () => {
+    window.open("https://github.com/MasonMacDonald")
 })
 
 function initializeView(){
